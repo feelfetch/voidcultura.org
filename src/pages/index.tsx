ef448855ed5c6370df5dfcb01bdee59ae3a94694
FEELFETCH <feelfetch@MacBook-Pro-di-FEELFETCH.local>
2022-07-31 19:43:11 +0200

    consistency desktop/mobile

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -20,7 +20,6 @@ const Home: NextPage = () => {
 
       <main className={[styles.main, ""].join(' ')}>
         <div data-scroll-section>
-
           <section id="firstSection" className={[styles.hero, "vh-100 dt w-100 bg-white relative z-0"].join(' ')} data-scroll data-scroll-speed="-8">
             <div className={[styles.heroContainer, "w-100 fixed top-1 center tc black ph3 ph4-l"].join(' ')}>
               <h1 className={[styles.heroHeadingText, "f6 fw6 tc"].join(' ')}><img src="/images/void.svg" alt="" className={[styles.hero, "logo"].join(' ')}/></h1>
@@ -45,7 +44,7 @@ const Home: NextPage = () => {
             </div>
           </section>
 
-          <section id="thirdSection" className={[styles.hero, "vh-50 vh-75-ns dt w-100 bg-white relative z-1"].join(' ')} data-scroll data-scroll-speed="2" data-scroll-target="#thirdSection">
+          <section id="thirdSection" className={[styles.hero, "vh-75 dt w-100 bg-white relative z-1"].join(' ')} data-scroll data-scroll-speed="2" data-scroll-target="#thirdSection">
             <div className={[styles.heroContainer, "dtc v-mid tc black ph3 ph4-l"].join(' ')}>
               <h1 className={[styles.heroHeadingText, "ttu f6 f2-m f-subheadline-l fw6 tc mv0"].join(' ')}><Link href="mailto:info@voidcultura.org">info@voidcultura.org</Link></h1>
               <h1 className={[styles.heroHeadingText, "ttu f6 f2-m f-subheadline-l fw6 tc mv0"].join(' ')}><Link href="https://instagram.com/voidcultura" passHref><a target="_blank" rel="noopener noreferrer">Instagram</a></Link> / <Link href="https://facebook.com/voidcultura"><a target="_blank" rel="noopener noreferrer">Facebook</a></Link></h1>
